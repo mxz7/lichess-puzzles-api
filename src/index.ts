d8d0b11889740711728336a69e34b40920c6f648
max <29782397+mxz7@users.noreply.github.com>
2026-04-05 18:29:53 +0100

    feat(logger): use loki transport

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,37 @@ import { puzzleRouter } from "./routes/puzzle.js";
 
 const app = new Hono();
 
+function buildTransport() {
+	if (
+		!process.env.LOKI_USERNAME ||
+		!process.env.LOKI_PASSWORD ||
+		!process.env.LOKI_HOST
+	) {
+		console.log("missing loki credentials, skipping loki transport");
+		return undefined;
+	}
+
+	console.log("using loki transport");
+
+	return {
+		target: "pino-loki",
+		options: {
+			host: process.env.LOKI_HOST,
+			basicAuth: {
+				username: process.env.LOKI_USERNAME,
+				password: process.env.LOKI_PASSWORD,
+			},
+			labels: { service_name: "lichess-puzzles-api" },
+			headers: { "X-Scope-OrgID": "max" },
+		},
+	};
+}
+
 app.use(
 	pinoLogger({
 		pino: {
 			level: "debug",
-			transport: {
-				target: "pino/file",
-				options: { destination: process.env.LOG_OUTPUT },
-			},
+			transport: buildTransport(),
 		},
 	}),
 );
